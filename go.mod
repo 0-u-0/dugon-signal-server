58726f1972eadc1b80ac8493cb6d689583efb77f
Chen Cong <craterone11@gmail.com>
2020-05-04 22:58:23 +0900

    add viper and example config

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gorilla/websocket v1.4.2
 	github.com/nats-io/nats.go v1.9.2
+	github.com/spf13/viper v1.6.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-import "github.com/0-u-0/dugon-signal-server/libs"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
 
 func main() {
-	clientGroup := libs.NewClientGroup()
-	go clientGroup.Run()
-	libs.InitWsServer(clientGroup)
+	viper.SetConfigName("config")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("/etc/dugon-signal-server/")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	port := viper.GetInt("port")
+	//httpsEnable := viper.GetBool("https_enable")
+	//cert := viper.GetString("cert")
+	//key := viper.GetString("key")
+	natsUrls := viper.GetStringSlice("nats_urls")
+
+
+	fmt.Println(port)
+	fmt.Println(natsUrls)
+
+	//clientGroup := libs.NewClientGroup()
+	//go clientGroup.Run()
+	//libs.InitWsServer(clientGroup)
 }
